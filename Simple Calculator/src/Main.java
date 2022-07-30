9a9d7eea7850b47c99819e20c0ed872ddedb0e8e
Mustafa Emre Uçaklı <73758831+mustafaemreucakli@users.noreply.github.com>
2022-07-30 20:09:10 +0300

    Add files via upload

---
+++ Simple Calculator/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double number1, number2, result = 0;
+        int operations;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the first number you want to operations.");
+        number1 = input.nextInt();
+        System.out.println("Please enter the second number you want to operations.");
+        number2 = input.nextInt();
+        System.out.println("Please select the operations\n1. Summation\n2. Substruction\n3. Multiplication\n4. Division\n5. Mod");
+        operations = input.nextInt();
+
+        switch (operations) {
+            case 1:
+                result = number1 + number2;
+                break;
+            case 2:
+                result = number1 - number2;
+                break;
+            case 3:
+                result = number1 * number2;
+                break;
+            case 4:
+                result = number1 / number2;
+                break;
+            case 5:
+                result = number1 % number2;
+        }
+
+        System.out.println("Result : " + result);
+
+    }
+
+}
