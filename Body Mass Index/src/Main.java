bbda7c9bf229c3b5654c491db4ada996fa86e269
Mustafa Emre Uçaklı <73758831+mustafaemreucakli@users.noreply.github.com>
2022-07-30 19:51:22 +0300

    Update Main.java
    Conditions added.

--- Body Mass Index/src/Main.java
+++ Body Mass Index/src/Main.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class Main {
 
+    static void bmiMethod(double a) {
+        if (a < 18.5) {
+            System.out.println("Underweight");
+        } else if (a >= 18.5 && a < 25) {
+            System.out.println("Normal");
+        } else if (a >= 25 && a < 30) {
+            System.out.println("Overweight");
+        } else if (a >= 30 && a < 35) {
+            System.out.println("Obese");
+        } else {
+            System.out.println("Extremely Obese");
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
@@ -14,6 +28,7 @@ public class Main {
         double bmi = weight / (height * height);
 
         System.out.println("Your Body Mass Index: " + bmi);
+        bmiMethod(bmi);
     }
 
 }
